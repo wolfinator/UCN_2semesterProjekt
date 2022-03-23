b12b43263149c73743d09febb6818925fd82de7a
Mads <54942401+wolfinator@users.noreply.github.com>
2022-03-23 13:44:25 +0100

    OrderCtrl opdateret, mangler kommentarer og exception handling

--- MiniProjekt_Persistens/src/ctrl/OrderCtrl.java
+++ MiniProjekt_Persistens/src/ctrl/OrderCtrl.java
@@ -1,12 +1,22 @@
 package ctrl;
 
+import db.OrderDbIF;
+import model.Customer;
+import model.Employee;
+import model.Order;
+import model.Product;
+import model.Receipt;
+import model.SaleLineItem;
+
 public class OrderCtrl {
 	
 	private CustomerCtrl customerCtrl;
 	private EmployeeCtrl employeeCtrl;
 	private ProductCtrl productCtrl;
 	
-	private OrderDb orderDb;
+	private OrderDbIF orderDb;
+	
+	private Order currentOrder;
 	
 	public OrderCtrl() {
 		customerCtrl = new CustomerCtrl();
@@ -17,19 +27,39 @@ public class OrderCtrl {
 	}
 	
 	public Order createOrder(int employeeId) {
-		
+		currentOrder = new Order();
+		Employee employee = employeeCtrl.findById(employeeId);
+		currentOrder.setEmployee(employee);
+		return currentOrder;
 	}
 	
 	public Customer findCustomer(String phoneNo) {
-		
+		Customer customer = customerCtrl.findByPhoneNo(phoneNo);
+		currentOrder.setCustomer(customer);
+		return customer;
 	}
 	
 	public SaleLineItem addProduct(String productNo, int quantity) {
+		Product product = productCtrl.findProductByNo(productNo);
 		
+		/*
+		 * Ved ikke om det er for weird at den returnerer
+		 * et SaleLineItem, men det står der pt i interaktionsdiagrammet
+		 */
+		SaleLineItem sli = new SaleLineItem();
+		sli.setAmount(quantity);
+		sli.setOrder(currentOrder);
+		sli.setProduct(product);
+		
+		currentOrder.addProduct(product, quantity);
+		
+		return sli;
 	}
 	
 	public Receipt endOrder() {
-		
+		Receipt receipt = new Receipt();
+		//TODO Find ud af hvad receipt skal indebære.
+		return receipt;
 	}
 	
 }
--- MiniProjekt_Persistens/src/model/Order.java
+++ MiniProjekt_Persistens/src/model/Order.java
@@ -1,11 +1,67 @@
 package model;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Order {
 	private Employee employee;
 	private Customer customer;
 	private LocalDate date;
 	private String orderNo;
+	private boolean deliveryStatus;
+	private LocalDate deliveryDate;
 	
+	public List<SaleLineItem> saleLineItems;
+	
+	public Order() {
+		saleLineItems = new ArrayList<>();
+	}
+	
+	public SaleLineItem addProduct(Product product, int quantity) {
+		SaleLineItem sli = new SaleLineItem();
+		
+		sli.setAmount(quantity);
+		sli.setProduct(product);
+		sli.setOrder(this);
+		
+		return sli;
+	}
+	
+	public Employee getEmployee() {
+		return employee;
+	}
+	public void setEmployee(Employee employee) {
+		this.employee = employee;
+	}
+	public Customer getCustomer() {
+		return customer;
+	}
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+	public LocalDate getDate() {
+		return date;
+	}
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+	public String getOrderNo() {
+		return orderNo;
+	}
+	public void setOrderNo(String orderNo) {
+		this.orderNo = orderNo;
+	}
+	public boolean isDeliveryStatus() {
+		return deliveryStatus;
+	}
+	public void setDeliveryStatus(boolean deliveryStatus) {
+		this.deliveryStatus = deliveryStatus;
+	}
+	public LocalDate getDeliveryDate() {
+		return deliveryDate;
+	}
+	public void setDeliveryDate(LocalDate deliveryDate) {
+		this.deliveryDate = deliveryDate;
+	}
 }
