a87540e8432049be507b64d381322a3abe185d26
Mads <54942401+wolfinator@users.noreply.github.com>
2022-03-23 12:53:51 +0100

    init model laget, mangler implementation på alle

---
+++ MiniProjekt_Persistens/src/model/Order.java
@@ -0,0 +1,11 @@
+package model;
+
+import java.time.LocalDate;
+
+public class Order {
+	private Employee employee;
+	private Customer customer;
+	private LocalDate date;
+	private String orderNo;
+	
+}
