2b7afb5e35910c37de74752b7b625f19aabd0d44
UnknownXQ <93514775+UnknownXQ@users.noreply.github.com>
2022-03-24 09:32:27 +0100

    ProductCtrl

---
+++ MiniProjekt_Persistens/src/ctrl/ProductCtrl.java
@@ -0,0 +1,17 @@
+package ctrl;
+
+import db.ProductDB;
+import db.ProductDbIF;
+import model.Product;
+
+public class ProductCtrl {
+	private ProductDB productDB; 
+	
+	public Product findProductByProductNo(String productNo) {
+		Product product = productDB;
+		
+		
+		return product;
+		
+	}
+}
