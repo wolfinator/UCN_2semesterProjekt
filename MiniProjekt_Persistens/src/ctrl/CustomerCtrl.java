94a32809bc0b254f4634f50ba5d88c8ac64725a0
KNUCN <93514744+KNUCN@users.noreply.github.com>
2022-03-24 09:02:30 +0100

    CustomerCtrl constructer

--- MiniProjekt_Persistens/src/ctrl/CustomerCtrl.java
+++ MiniProjekt_Persistens/src/ctrl/CustomerCtrl.java
@@ -6,9 +6,20 @@ import model.Customer;
 public class CustomerCtrl {
 	private CustomerDB customerDB;
 	
-
+	public CustomerCtrl(CustomerDB customerDB) {
+		this.customerDB = customerDB;
+	}
+	
 	public Customer findByPhoneNo(String phoneNo) {
 		Customer res = CustomerDB.getInstance().findByPhoneNo(phoneNo);
 		return res;
+	}
+
+	public CustomerDB getCustomerDB() {
+		return customerDB;
+	}
+
+	public void setCustomerDB(CustomerDB customerDB) {
+		this.customerDB = customerDB;
 	}
 }
