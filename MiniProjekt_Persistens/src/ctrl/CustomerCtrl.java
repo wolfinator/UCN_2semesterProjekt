fac6078bd06f492a020a634adedf92ff032eeb0e
KNUCN <93514744+KNUCN@users.noreply.github.com>
2022-03-24 13:31:25 +0100

    Ændringer på CustomerDbIF, Customer, CustomerCTtrl, mangler at færdiggøre CustomerDB

--- MiniProjekt_Persistens/src/ctrl/CustomerCtrl.java
+++ MiniProjekt_Persistens/src/ctrl/CustomerCtrl.java
@@ -1,17 +1,34 @@
 package ctrl;
 
-import db.CustomerDb;
+import java.util.List;
+
+import db.CustomerDB;
 import model.Customer;
 
 public class CustomerCtrl {
-	private CustomerDb customerDb;
-	
-	public CustomerCtrl() {
-		customerDb = new CustomerDb();
-	}
-	
-	public Customer findByPhoneNo(String phoneNo) {
-		return null;
+	private CustomerDB customerDB;
 
+	public CustomerCtrl() throws DataAccessException {
+		customerDB = new CustomerDB();
+	}
+
+	public List<Customer> findByPhoneNo(String phoneNo) throws DataAccessException {
+		List<Customer> res = customerDB.findByPhoneNo(phoneNo, true);
+		return res;
+	}
+
+	public Customer findByPersonID(int personID, boolean b) throws DataAccessException {
+		Customer res = customerDB.findByPersonID(true);
+		return res;
+	}
+
+	public List<Customer> findCustomer(boolean fullAssociation) throws DataAccessException {
+		List<Customer> res = customerDB.FindCustomer();
+		return res;
+	}
+
+	public Customer insert(Customer customer) throws DataAccessException {
+		Customer res = customerDB.insert(customer);
+		return res;
 	}
 }
