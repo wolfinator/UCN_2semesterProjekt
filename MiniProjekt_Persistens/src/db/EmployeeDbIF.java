0a09a17771088e4f59135e2130cd6e426a1fe436
LessIsMore42069 <93514785+LessIsMore42069@users.noreply.github.com>
2022-03-24 13:28:59 +0100

    EmployeeDB  - lavet

--- MiniProjekt_Persistens/src/db/EmployeeDbIF.java
+++ MiniProjekt_Persistens/src/db/EmployeeDbIF.java
@@ -1,5 +1,10 @@
 package db;
 
+import java.util.List;
+
+import ctrl.DataAccessException;
+import model.Employee;
+
 public interface EmployeeDbIF {
-	
+	Employee findEmployeeById(int id) throws DataAccessException;
 }
