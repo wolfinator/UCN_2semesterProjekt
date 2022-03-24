ceb796f7ab39c5ce32984a1e1b4a4635cea25226
Mads <54942401+wolfinator@users.noreply.github.com>
2022-03-24 14:24:26 +0100

    added get/set for id in person

--- MiniProjekt_Persistens/src/model/Person.java
+++ MiniProjekt_Persistens/src/model/Person.java
@@ -1,6 +1,7 @@
 package model;
 
 public abstract class Person {
+	private int id;
 	private String name;
 	private String email;
 	private String phoneNo;
@@ -22,6 +23,14 @@ public abstract class Person {
 		
 	}
 	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
 	public String getName() {
 		return name;
 	}
