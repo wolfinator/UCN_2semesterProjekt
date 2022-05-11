49c9a4883d38024965fd07b2a68c5b4791917688
Annafalgren <10407493@ucn.dk>
2022-05-11 11:09:03 +0200

    Update person

--- foersteaarsprojekt/src/model/Person.java
+++ foersteaarsprojekt/src/model/Person.java
@@ -1,5 +1,59 @@
 package model;
 
 public abstract class Person {
+	private int id;
+	private String name;
+	private String phoneNo;
+	private String email;
+	private String address;
+
+	public Person(int id, String name, String phoneNo, String email, String address) {
+
+		this.id = id;
+		this.name = name;
+		this.phoneNo = phoneNo;
+		this.email = email;
+		this.address = address;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhoneNo() {
+		return phoneNo;
+	}
+
+	public void setPhoneNo(String phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
 
 }
