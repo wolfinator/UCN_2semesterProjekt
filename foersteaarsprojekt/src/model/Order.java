39e71564655bd605eda20487765344761a3c57cc
UnknownXQ <93514775+UnknownXQ@users.noreply.github.com>
2022-05-11 11:28:29 +0200

    Mangler lidt ændringer

--- foersteaarsprojekt/src/model/Order.java
+++ foersteaarsprojekt/src/model/Order.java
@@ -13,7 +13,30 @@ public class Order {
 	}
 	
 	public SaleLineItem addProduct(Product product, int quantity) {
+		SaleLineItem sli = new SaleLineItem();
 		
+		sli.setProduct(); 
+		sli.setQuanity();
+		sli.setOrder(this); 
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public boolean isStatus() {
+		return status;
+	}
+
+	public void setStatus(boolean status) {
+		this.status = status;
 	}
 	
+	public List<OrderLineItem> getOrderLineItem(){
+		return orderLineItems; 
+	}
 }
