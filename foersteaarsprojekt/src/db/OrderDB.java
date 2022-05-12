8f8ffece11385e40ee318b8a8bf3db4653369baf
UnknownXQ <93514775+UnknownXQ@users.noreply.github.com>
2022-05-12 12:08:01 +0200

    Update OrderDB.java

--- foersteaarsprojekt/src/db/OrderDB.java
+++ foersteaarsprojekt/src/db/OrderDB.java
@@ -1,5 +1,29 @@
 package db;
 
-public class OrderDB implements OrderDBIF{
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+import ctrl.DataAccessException;
+
+public class OrderDB implements OrderDBIF{
+	private Connection con; 
+	private static final String SAVE_ORDER_SQL = "select int id, int orderNo, boolean status"; //TODO -> ??
+	private PreparedStatement ps_saveOrder; 
+
+	public OrderDB() throws DataAccessException {
+		init(); 
+	}
+
+	private void init() throws DataAccessException {
+		// TODO Auto-generated method stub
+		try {
+			con = DBConnection.getInstance().getConnection(); 
+			ps_saveOrder = con.prepareStatement(SAVE_ORDER_SQL); 
+		}catch(DataAccessException e) {
+			e.printStackTrace();
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
 }
