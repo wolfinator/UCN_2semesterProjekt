709f410636c32f6cb2ba1b619a2aa757a0ef34d6
Annafalgren <10407493@ucn.dk>
2022-05-12 13:15:52 +0200

    Table

--- foersteaarsprojekt/src/model/Table.java
+++ foersteaarsprojekt/src/model/Table.java
@@ -1,5 +1,20 @@
 package model;
 
 public class Table {
-
+	private int tableNo;
+	private int seats;
+	
+	public int getTableNo() {
+		return tableNo;
+	}
+	public void setTableNo(int tableNo) {
+		this.tableNo = tableNo;
+	}
+	public int getSeats() {
+		return seats;
+	}
+	public void setSeats(int seats) {
+		this.seats = seats;
+	}
+	
 }
