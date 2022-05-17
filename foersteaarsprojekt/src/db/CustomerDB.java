23ff5a493b9d2c237b4570b824f43daa9f7ff9e9
Mads <54942401+wolfinator@users.noreply.github.com>
2022-05-17 09:36:03 +0200

    added ability to return generated keys

--- foersteaarsprojekt/src/db/CustomerDB.java
+++ foersteaarsprojekt/src/db/CustomerDB.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import ctrl.DataAccessException;
 import model.Customer;
@@ -27,7 +28,7 @@ public class CustomerDB implements CustomerDBIF {
 		Connection con = DBConnection.getInstance().getConnection();
 		try {
 			ps_findById = con.prepareStatement(FIND_BY_ID_SQL);
-			ps_saveId = con.prepareStatement(SAVE_SQL);
+			ps_saveId = con.prepareStatement(SAVE_SQL, Statement.RETURN_GENERATED_KEYS);
 		} catch (SQLException e) {
 			e.printStackTrace(); // TODO not Finished
 		}
@@ -56,7 +57,7 @@ public class CustomerDB implements CustomerDBIF {
 	@Override
 	public int saveCustomer(Customer customer) throws DataAccessException {
 		int customerId = -1;
-		dbc.startTransaction();
+
 		try {
 			ps_saveId.setString(1, customer.getFirstName());
 			ps_saveId.setString(2, customer.getLastName());
@@ -64,21 +65,14 @@ public class CustomerDB implements CustomerDBIF {
 			ps_saveId.setString(4, customer.getEmail());
 			ps_saveId.executeUpdate();
 			
-			dbc.commitTransaction();
+			ResultSet generatedKeys = ps_saveId.getGeneratedKeys();
+			if(generatedKeys.next()) customerId = generatedKeys.getInt(1);
 			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
-			dbc.rollbackTransaction();
+
 			e.printStackTrace();
 		}	
-		
-		try {
-			ResultSet generatedKeys = ps_saveId.getGeneratedKeys();
-			if(generatedKeys.next()) customerId = generatedKeys.getInt(1);
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 		
 		// TODO insert customer in db and return inserted id
 		return customerId;
