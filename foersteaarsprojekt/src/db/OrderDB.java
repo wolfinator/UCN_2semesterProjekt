af7fd224e9ad394e06cacee0ee1a0c32f5de409a
LessIsMore42069 <93514785+LessIsMore42069@users.noreply.github.com>
2022-05-20 10:34:29 +0200

    SaveOrder() endelig laves succesRate høj!

--- foersteaarsprojekt/src/db/OrderDB.java
+++ foersteaarsprojekt/src/db/OrderDB.java
@@ -7,33 +7,39 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.time.LocalDate;
+import java.util.List;
 
 import ctrl.DataAccessException;
 import model.Order;
+import model.OrderLineItem;
 import model.Reservation;
 
-public class OrderDB implements OrderDBIF{
-	private Connection con; 
-	private static final String SAVE_ORDER_SQL =  "INSERT INTO _Order(totalPrice, status, tableId, reservationId "
+public class OrderDB implements OrderDBIF {
+	private Connection con;
+	private static final String SAVE_ORDER_SQL = "INSERT INTO _Order(totalPrice, status, tableId, reservationId) "
 			+ "values (?,?,?,?);";
-	
-	private PreparedStatement ps_saveOrder; 
+	private String INSERT_ORDERLINEITEM_SQL = "INSERT INTO OrderLineItem (OrderNo, productId, quantity,)"
+			+ " values (?,?,?);";
+
+	private PreparedStatement ps_saveOrder;
 
 	public OrderDB() throws DataAccessException {
-		init(); 
+		init();
 	}
 
 	private void init() throws DataAccessException {
 		// TODO Auto-generated method stub
 		try {
-			con = DBConnection.getInstance().getConnection(); 
-			ps_saveOrder = con.prepareStatement(SAVE_ORDER_SQL); 
-		}catch(DataAccessException e) {
+			con = DBConnection.getInstance().getConnection();
+			ps_saveOrder = con.prepareStatement(SAVE_ORDER_SQL);
+			ps_saveOrder = con.prepareStatement(INSERT_ORDERLINEITEM_SQL);
+		} catch (DataAccessException e) {
 			e.printStackTrace();
-		}catch(SQLException e) {
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
+
 	@Override
 	public boolean saveOrder (Order o, Reservation r)  throws DataAccessException {
 	boolean res = true;
@@ -54,10 +60,31 @@ public class OrderDB implements OrderDBIF{
 		ps_saveOrder.executeUpdate();
 		
 		ResultSet generatedKeys = ps_saveOrder.getGeneratedKeys();
-		if(generatedKeys.next()) r.setOrder(o);
+		if(generatedKeys.next()) o.setId(generatedKeys.getInt(1));
 		
+	} catch (SQLException e) {
+		// TODO: handle exception
+		e.printStackTrace();
+	}		
 		
-	}catch (SQLException e) {
+	
+	//Inserting OrderLineItem
+		try {
+		List<OrderLineItem> oli = o.getOrderLineItem();
+		//Looping all orderLineItems, and executing queries
+		for(OrderLineItem orderLineItem : oli) {
+			ps_saveOrder = con.prepareStatement(INSERT_ORDERLINEITEM_SQL);
+			
+			ps_saveOrder.setInt(1, o.getOrderNo());
+			ps_saveOrder.setInt(2, orderLineItem.getProduct().getId());
+			ps_saveOrder.setInt(3, orderLineItem.getQuantity());
+			
+			ps_saveOrder.execute();
+		}
+		
+		}
+		
+	  catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
