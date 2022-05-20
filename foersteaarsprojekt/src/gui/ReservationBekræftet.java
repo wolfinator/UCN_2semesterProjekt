5ef503540138f6f91ceac7716a8d5b4d4146ba51
Annafalgren <10407493@ucn.dk>
2022-05-20 10:46:46 +0200

    Update ReservationBekræftet.java

--- foersteaarsprojekt/src/gui/ReservationBekræftet.java
+++ foersteaarsprojekt/src/gui/ReservationBekræftet.java
@@ -30,18 +30,18 @@ public class ReservationBekræftet extends JFrame {
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					ReservationBekræftet frame = new ReservationBekræftet();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+//	public static void main(String[] args) {
+//		EventQueue.invokeLater(new Runnable() {
+//			public void run() {
+//				try {
+//					ReservationBekræftet frame = new ReservationBekræftet();
+//					frame.setVisible(true);
+//				} catch (Exception e) {
+//					e.printStackTrace();
+//				}
+//			}
+//		});
+//	}
 
 	/**
 	 * Create the frame.
